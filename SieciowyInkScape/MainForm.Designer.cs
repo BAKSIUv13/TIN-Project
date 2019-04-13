f722538bf2d0b81f5ef2a7babb83f883b8ec8d80
Norbert <npietruc@mion.elka.pw.edu.pl>
2019-04-13 19:47:03 +0200

    Initial commit

---
+++ SieciowyInkScape/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SieciowyInkScape
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.chatPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.chatPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chatBox
+            // 
+            this.chatBox.Location = new System.Drawing.Point(3, 21);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(295, 339);
+            this.chatBox.TabIndex = 0;
+            // 
+            // chatPanel
+            // 
+            this.chatPanel.Controls.Add(this.messageBox);
+            this.chatPanel.Controls.Add(this.label2);
+            this.chatPanel.Controls.Add(this.label1);
+            this.chatPanel.Controls.Add(this.chatBox);
+            this.chatPanel.Location = new System.Drawing.Point(632, 12);
+            this.chatPanel.Name = "chatPanel";
+            this.chatPanel.Size = new System.Drawing.Size(301, 426);
+            this.chatPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chat:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Napisz wiadomość:";
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(3, 379);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(295, 20);
+            this.messageBox.TabIndex = 3;
+            this.messageBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.messageBox_KeyPressed);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 450);
+            this.Controls.Add(this.chatPanel);
+            this.Name = "MainForm";
+            this.Text = "Sieciowy InkScape - Norbert Pietrucha-Kacprowicz";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.chatPanel.ResumeLayout(false);
+            this.chatPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.Panel chatPanel;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SieciowyInkScape/MainForm.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SieciowyInkScape
+{
+    public partial class MainForm : Form
+    {
+        enum ErrorCodes
+        {
+            BAD_HEADER, PACKET_TOO_LONG, SEND_TIMEDOUT
+        }
+        struct Codes // po konwersji littleendian -> bigendian litery będą zamienione
+        {
+            public const UInt32 OwO = ('O' << 24 | 'w' << 16 | 'O' << 8 | '!' << 0); // OwO!
+
+            public const UInt32 MessageOutbound = ('m' << 24 | 's' << 16 | 'g' << 8 | '0' << 0); // msg0
+            public const UInt32 MessageInbound = ('m' << 24 | 's' << 16 | 'g' << 8 | '1' << 0); // msg1
+        }
+
+        public class MessageCreator
+        {
+            MainForm parent;
+
+            public MessageCreator(MainForm parent)
+            {
+                this.parent = parent;
+            }
+
+            public byte[] SendChatMessage(string message)
+            {
+                byte[] toRet = new byte[4 + 4 + message.Length]; // msg0(byte[4])|length(int32)|message(char[])
+                Array.Copy(parent.SocketSetUInt32(Codes.MessageOutbound), 0, toRet, 0, 4);
+                Array.Copy(parent.SocketSetUInt32((uint)message.Length), 0, toRet, 4, 4);
+                Array.Copy(parent.SocketSetString(message), 0, toRet, 8, message.Length);
+
+                return toRet;
+            }
+        }
+
+        byte[] buf;
+        Socket socket;
+        Thread socketThread;
+        MessageCreator messageCreator;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            messageCreator = new MessageCreator(this);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Show();
+
+            bool retry;
+
+            do
+            {
+                retry = false;
+
+                LoginForm loginForm = new LoginForm();
+                loginForm.ShowDialog();
+
+                if (loginForm.DialogResult == DialogResult.OK)
+                {
+                    if (Connect(loginForm.hostname, loginForm.port) == false)
+                    {
+                        MessageBox.Show("Połączenie z serwerem nieudane", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        retry = true;
+                    }
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            while (retry);
+
+
+        }
+
+        void MakeError(ErrorCodes errorCode)
+        {
+            switch(errorCode)
+            {
+                case ErrorCodes.BAD_HEADER:
+                    throw new Exception("Zły nagłówek komunikatu!");
+                    break;
+                case ErrorCodes.SEND_TIMEDOUT:
+                    throw new Exception("Wysłanie komunikatu dostało timeout!");
+                    break;
+                case ErrorCodes.PACKET_TOO_LONG:
+                    throw new Exception("Pakiet był za długi!");
+                    break;
+            }
+        }
+
+        bool Connect(string server, int port)
+        {
+            try
+            {
+                buf = new byte[4096];
+                socket = ConnectSocket(server, port);
+                if (socket == null) return false;
+
+                socket.ReceiveTimeout = 1000;
+                socket.SendTimeout = 1000;
+
+
+
+                socketThread = new Thread(new ThreadStart(SocketThread));
+                socketThread.Start();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+           
+        }
+
+        void SocketReceive(uint size)
+        {
+            if(size > 1024*1024) MakeError(ErrorCodes.PACKET_TOO_LONG);
+
+            while (size > buf.Length) buf = new byte[buf.Length * 2];
+
+            bool retry;
+
+            do
+            {
+                retry = false;
+
+                try
+                {
+                    socket.Receive(buf, (int)size, SocketFlags.None);
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.ErrorCode == 10060)
+                    {
+                        retry = true;
+                        // operation timedout, OK
+                    }
+                    else
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            while (retry);
+
+
+        }
+
+        void SocketSend(byte[] message)
+        {
+            try
+            {
+                socket.Send(SocketSetUInt32(Codes.OwO));
+                socket.Send(message);
+            }
+            catch (SocketException ex)
+            {
+                if (ex.ErrorCode == 10060)
+                {
+                    MakeError(ErrorCodes.SEND_TIMEDOUT);
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        byte[] SocketSetUInt32(UInt32 value)
+        {
+            byte[] toRet = System.BitConverter.GetBytes(value);
+            if (System.BitConverter.IsLittleEndian) Array.Reverse(toRet);
+
+            return toRet;
+        }
+        byte[] SocketSetString(string value)
+        {
+            byte[] toRet = Encoding.ASCII.GetBytes(value);
+
+            return toRet;
+        }
+
+        UInt32 SocketGetUInt32()
+        {
+            if (System.BitConverter.IsLittleEndian) Array.Reverse(buf, 0, 4);
+            return System.BitConverter.ToUInt32(buf, 0);
+        }
+        string SocketGetString(uint size)
+        {
+            return Encoding.ASCII.GetString(buf, 0, (int)size);
+        }
+
+        void SocketThread()
+        {
+            while(true)
+            {
+                SocketReceive(4);
+                UInt32 header = SocketGetUInt32();
+                if(header == Codes.OwO) // początek komunikatu
+                {
+                    SocketReceive(4);
+                    UInt32 messageType = SocketGetUInt32();
+
+                    switch(messageType)
+                    {
+                        case Codes.MessageInbound:
+                            UInt32 usernameLength;
+                            string username;
+                            UInt32 messageLength;
+                            string message;
+
+                            SocketReceive(4);
+                            usernameLength = SocketGetUInt32();
+                            SocketReceive(usernameLength);
+                            username = SocketGetString(usernameLength);
+                            SocketReceive(4);
+                            messageLength = SocketGetUInt32();
+                            SocketReceive(messageLength);
+                            message = SocketGetString(messageLength);
+
+                            chatBox.Invoke(new MethodInvoker(delegate ()
+                            {
+                                chatBox.AppendText(username + ": " + message + "\n");
+                            }));
+
+                            break;
+                    }
+                }
+                else
+                {
+                    MakeError(ErrorCodes.BAD_HEADER);
+                }
+            }
+            
+
+        }
+
+        private static Socket ConnectSocket(string server, int port)
+        {
+            Socket s = null;
+            IPHostEntry hostEntry = null;
+
+            // Get host related information.
+            hostEntry = Dns.GetHostEntry(server);
+
+            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
+            // an exception that occurs when the host IP Address is not compatible with the address family
+            // (typical in the IPv6 case).
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                try
+                {
+                    IPEndPoint ipe = new IPEndPoint(address, port);
+                    Socket tempSocket =
+                        new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    tempSocket.Connect(ipe);
+
+                    if (tempSocket.Connected)
+                    {
+                        s = tempSocket;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            return s;
+        }
+
+        private void messageBox_KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                SocketSend(messageCreator.SendChatMessage(messageBox.Text));
+                chatBox.AppendText("Ja: " + messageBox.Text + "\n");
+                messageBox.Clear();
+            }
+        }
+    }
+}
