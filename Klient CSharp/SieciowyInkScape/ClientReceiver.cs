3e7da08df164c2ebd610775df14e2906b4ec43f2
Norbert <npietruc@mion.elka.pw.edu.pl>
2019-06-04 20:48:00 +0200

    Test commit

--- Klient CSharp/SieciowyInkScape/ClientReceiver.cs
+++ Klient CSharp/SieciowyInkScape/ClientReceiver.cs
@@ -123,8 +123,9 @@ namespace SieciowyInkScape
                                 UInt32 usernameLength;
                                 string username;
 
-                                usernameLength = SocketReceiveUInt32();
-                                username = SocketReceiveString(usernameLength);
+                                // usernameLength = SocketReceiveUInt32();
+                                // username = SocketReceiveString(usernameLength);
+                                username = SocketReceiveString(6);
 
                                 mainForm.Invoke(new Action(() =>
                                 {
